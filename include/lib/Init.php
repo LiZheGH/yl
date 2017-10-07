a3aa65479d2023532fc91fe826f80c78bb39c2e8
lizhe <li_zhe@aliyun.com>
2017-10-08 02:38:48 +0800

    4

--- include/lib/Init.php
+++ include/lib/Init.php
@@ -1,5 +1,4 @@
 <?php
-var_dump($_SERVER['HTTP_HOST']);exit;
 define ( 'ENV_LOCAL', 1 ); // 内网环境
 define ( 'ENV_DEVEL', 2 ); // 测试环境（初步判断条件为Windows,NT开头的域名访问）
 define ( 'ENV_OUTER', 4 ); // 外网生产环境，正常访问环境
@@ -9,15 +8,22 @@ ini_set ( 'date.timezone', 'Asia/Shanghai' );
 
 
 if(isset($_SERVER['HTTP_HOST'])) {
-    if ($_SERVER['HTTP_HOST'] == 'http://yl.codeboxs.com/')
-	//正式环境
-	define ( 'DOMAIN', 'www.yl.com' );
-	define ( 'DB_PASSWORD', '123abc!@#' );
-	define ('WEIXIN_APP_ID', 'wx1c395d9d143e9ad2');
+    if ($_SERVER['HTTP_HOST'] == 'yl.codeboxs.com'){
+        //正式环境
+        define ( 'DOMAIN', 'yl.codeboxs.com' );
+        define ( 'DB_USERNAME', 'api' );
+        define ( 'DB_PASSWORD', 'api3306' );
+        define ( 'DB_DBNAME', 'yl' );
+    } else {
+        //正式环境
+        define ( 'DOMAIN', 'www.yl.com' );
+        define ( 'DB_USERNAME', 'root' );
+        define ( 'DB_PASSWORD', '123abc!@#' );
+        define ( 'DB_DBNAME', 'yl' );
+    }
+
 }
 define ( 'DB_SERVERNAME', '127.0.0.1' );
-define ( 'DB_USERNAME', 'root' );
-define ( 'DB_DBNAME', 'yl' );
 
 define ( 'SYSTEM_PATH', dirname ( __FILE__ ) );
 define ( 'BASE_DIR', substr ( SYSTEM_PATH, 0, - 11 ) );
